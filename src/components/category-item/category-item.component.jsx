2cd3c53434e9388a077c24325b799ef44ad47a6d
Max <imaksmula@gmail.com>
2023-05-29 18:07:14 -0500

    routes & prettier

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,21 +1,22 @@
-import './category-item.styles.scss'
+import "./category-item.styles.scss";
 
 const CategoryItem = ({ category }) => {
+   const { id, title, imageUrl } = category;
 
-    const { id, title, imageUrl } = category
-
-    return (
-        <div key={id} className="category-container">
-          <div className="background-image" style={{
-              backgroundImage: `url(${imageUrl})`
+   return (
+      <div key={id} className="category-container">
+         <div
+            className="background-image"
+            style={{
+               backgroundImage: `url(${imageUrl})`,
             }}
-          />
-          <div className="category-body-container">
+         />
+         <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
-          </div>  
-        </div>
-    )
-}
+         </div>
+      </div>
+   );
+};
 
 export default CategoryItem;
