e89df698323c8ba41c01bc456e05daa8197a681f
Max Mula <maxmula@Maxs-MacBook-Pro.local>
2024-03-12 19:01:35 -0500

    Finished Authentication page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/unthentication/authentication.component';
 
 const Shop = () => {
    return <div>Shop component</div>;
@@ -12,8 +12,8 @@ const App = () => {
       <Routes>
          <Route path="/" element={<Navigation />}>
             <Route index element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="auth" element={<Authentication />} />
          </Route>
       </Routes>
    );
